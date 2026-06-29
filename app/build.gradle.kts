6cdf9c045caae6ecd62f20d79d1b1be649479eb6
denkkn <denkkn@github.com>
2026-06-29 21:53:27 +0800

    fix: copy keystore to default location, remove duplicate signingConfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,6 @@ android {
     namespace = "com.example.encryptedchat"
     compileSdk = 34
 
-    signingConfigs {
-        create("debug") {
-            storeFile = rootProject.file("debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
-
     defaultConfig {
         applicationId = "com.example.encryptedchat"
         minSdk = 26
@@ -25,12 +16,8 @@ android {
     }
 
     buildTypes {
-        debug {
-            signingConfig = signingConfigs.getByName("debug")
-        }
         release {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -43,29 +30,17 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    kotlinOptions { jvmTarget = "1.8" }
 
-    buildFeatures {
-        viewBinding = true
-    }
+    buildFeatures { viewBinding = true }
 }
 
 dependencies {
-    // AndroidX
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.core:core-ktx:1.12.0")
-
-    // Material Design (BottomNavigationView)
     implementation("com.google.android.material:material:1.11.0")
-
-    // Networking
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
